69f9d36d8a32ae0098c354ceef521a0768d2ec95
mohanssingh002 <mohanssingh002@users.noreply.github.com>
2026-05-30 14:34:45 +0530

    Add Chapter 8 - Switch Statement

---
+++ Chapter_08_switch_statement/61_nobreak.js
@@ -0,0 +1,27 @@
+let day = 3;
+switch (day) {
+    case 1:
+        console.log("monday");
+
+    case 2:
+        console.log("tuesday");
+
+
+    case 3:
+        console.log("wednesday");
+
+    case 4:
+        console.log("thursday");
+
+    case 5:
+        console.log("friday");
+
+    case 6:
+        console.log("saturday");
+
+    case 7:
+        console.log("Sunday");
+
+    default:
+        console.log("not sure")
+}
